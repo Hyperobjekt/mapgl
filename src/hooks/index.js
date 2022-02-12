1e147340969df19adde3c18bf688c7eb5e377d3b
lane <laneolson@gmail.com>
2022-02-12 13:10:22 -0800

    feat: add flyTo hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,17 @@
+import useMapState from "./useMapState";
+
 export { default as useMapStore } from "./useMapStore";
+export { useMapState };
+
+export function useMapFlyTo() {
+  return useMapState("flyTo");
+}
+export function useMapFlyToFeature() {
+  return useMapState("flyToFeature");
+}
+export function useMapFlyToBounds() {
+  return useMapState("flyToBounds");
+}
+export function useMapFlyToDefault() {
+  return useMapState("flyToDefault");
+}
